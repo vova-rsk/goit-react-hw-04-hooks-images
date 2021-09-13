ee999ebd489d1efd7e9bb52d32ebe5c857862693
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-13 16:22:58 +0300

    fix

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,8 +1,8 @@
 const KEY = '22441039-e3c3a22ef42346706974d6393';
+const BASE_URL = 'https://pixabay.com/api/';
 const PER_PAGE = 12;
 
 const fetchImages = (query, page) => {
-  const baseUrl = 'https://pixabay.com/api/';
   const searchParams = new URLSearchParams({
     q: query,
     page,
@@ -12,7 +12,7 @@ const fetchImages = (query, page) => {
     per_page: PER_PAGE,
   });
 
-  return fetch(`${baseUrl}?${searchParams.toString()}`).then(response => {
+  return fetch(`${BASE_URL}?${searchParams.toString()}`).then(response => {
     if (!response.ok) {
       return Promise.rejected(new Error('Oops, something went wrong!'));
     }
